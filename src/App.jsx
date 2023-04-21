2ae395f4d939dd41ec2a35d077ef89b031df94e9
patsicko <patsicko@g>
2023-04-22 01:59:33 +0200

    ch-setup-tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div className='bg-red-200 p-40 w-1/2 m-20 rounded-xl'>
+    <h1 className='text-green-700 h-300 border p-20 ml-10 mt-7 justify-center shadow-xl w-1/2 m-500'>REACT WITH VITE+TAILWIND</h1>
+   </div>
+  )
+}
+
+export default App
