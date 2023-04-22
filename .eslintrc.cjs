5a49e1302cb1a01b07d570b1ba78f0a48de45651
patsicko <patsicko@g>
2023-04-22 12:00:49 +0200

    ch-configure-eslint - configure eslint - configure prettier [Finishes#]

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,29 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "react/react-in-jsx-scope": "off",
+  },
+};
